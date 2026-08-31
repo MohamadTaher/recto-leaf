b6d88ac9b2059635e21a82fd85d7aeb79f89c446
Mohamad Taher <tahr33288@gmail.com>
2026-08-30 22:46:12 -0600

    leaf: fix import order in the speech panel

--- app/src/main/java/leaf/novel/presentation/reader/NovelSpeechPanel.kt
+++ app/src/main/java/leaf/novel/presentation/reader/NovelSpeechPanel.kt
@@ -2,6 +2,7 @@ package leaf.novel.presentation.reader
 
 import android.content.Context
 import android.media.AudioManager
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -12,7 +13,6 @@ import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Icon
